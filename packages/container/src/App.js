935bf2488d75cbe2a676adf5d679bfef316c968b
kangchuming <kangchum@163.com>
2024-04-29 22:03:37 +0800

    feat:添加gitignore

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MarketingMount from './components/MarketingApp';
+
+
+export default () => {
+  return <div>
+    <h1>Hi there</h1>
+    <br />
+    <MarketingMount />
+  </div>
+}
