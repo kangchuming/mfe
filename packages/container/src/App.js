a9c9ab8c65e9fdd7ac30ed7418b0a4f109f68b80
kangchuming <kangchum@163.com>
2024-05-17 22:59:20 +0800

    add classNamePrex

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider,  createGenerateClassName} from '@material-ui/core/styles';
+
+const createGenerateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+})
+
 export default () => {
   return (
     <BrowserRouter>
+    <StylesProvider createGenerateClassName={createGenerateClassName}>
       <div>
         <Header />
         <MarketingApp />
       </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
